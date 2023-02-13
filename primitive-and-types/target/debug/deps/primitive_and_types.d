41f8804ca507c30e898f33f8768088cddaad2113
Mohammad Javad Zallaghi <MohammadJavadZallaghi@gmail.com>
2023-02-13 18:04:17 +0330

    primitives and different types in rust

---
+++ primitive-and-types/target/debug/deps/primitive_and_types.d
@@ -0,0 +1,5 @@
+D:\Javad\Rust Learning\rust-learning\primitive-and-types\target\debug\deps\primitive_and_types.exe: src\main.rs
+
+D:\Javad\Rust Learning\rust-learning\primitive-and-types\target\debug\deps\primitive_and_types.d: src\main.rs
+
+src\main.rs:
---
+++ primitive-and-types/target/debug/primitive-and-types.d
@@ -0,0 +1,1 @@
+D:\Javad\Rust\ Learning\rust-learning\primitive-and-types\target\debug\primitive-and-types.exe: D:\Javad\Rust\ Learning\rust-learning\primitive-and-types\src\main.rs
